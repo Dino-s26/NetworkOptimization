6a3ceb9cfeb8a3abaff9123a178efed2b56a290d
Dino <alldino.syaman@gmail.com>
2018-09-15 01:32:59 +0700

    Revert "Add Base File"
    This reverts commit 46a477776f91dca2a14cc5b008fdb7e3d51123fa.

--- Capture Automation/credential.py
+++
@@ -1,7 +0,0 @@
-## 										Credentials 				    				 ##
-## ------------------------------------------------------------------------------------- ##
-## This file are the Username & Password that I used to login (SSH) to the Cisco Devices ##
-## Please change it to suit your need 													 ##
-
-usern = 'admin'
-passwd = 'admin'
