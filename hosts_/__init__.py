efbbab3d6d78bfe51b2e6c62971df82831b39184
Dino <alldino.syaman@gmail.com>
2019-05-01 23:07:17 +0700

    add host folder
    add host folder including __init__.py and host_data_dummy for the template host

---
+++ hosts_/__init__.py
@@ -0,0 +1,1 @@
+from .host_data_dummy import *
---
+++ hosts_/host_data_dummy.py
@@ -0,0 +1,4 @@
+##Host Gather##
+list_ip = [
+           '127.0.0.1' #Change this ip to your server / network device ip, add ',' / comma for multiple ip
+       ]
